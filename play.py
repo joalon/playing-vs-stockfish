2225f7bf14895bac9b6f10e7f0cbc399bfc1c688
joalon <joakimlonnegren@gmail.om>
2020-01-08 20:39:22 -0300

    Adds a simple board representation based on FEN

--- play.py
+++ play.py
@@ -4,18 +4,46 @@
 from stockfish import Stockfish
 
 
+fen_string = ""
+with open('start.fen', 'r') as f:
+    fen_string = f.readline()
+
+print(fen_string)
 
 pieces = {
-    'White king': "\u265A",
-    'Black king': "\u2654",
+    'K': "\u265A",
+    'Q': "\u265B",
+    'R': "\u265C",
+    'B': "\u265D",
+    'N': "\u265E",
+    'P': "\u265F",
+
+    'k': "\u2654",
+    'q': "\u2655",
+    'r': "\u2656",
+    'b': "\u2657",
+    'n': "\u2658",
+    'p': "\u2659"
 }
 
+def print_board(fen_string):
+    positions, _, _, _, _, _ = fen_string.split(' ')
+    ranks = positions.split('/')
 
+    global pieces
+
+    for rank in ranks:
+        for piece in rank:
+            if piece in pieces:
+                print(pieces[piece], end=' ')
+            else:
+                print('. ' * int(piece), end=' ')
+        print()
+
+print_board(fen_string)
 
 stockfish = Stockfish()
-
 current_position = []
-
 while True:
 
     while not stockfish.is_move_correct(white_next_move := input("Whites next move: ")):
